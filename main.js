49d89e3d85f04ae24cce7a1428d3d904e5d1453b
Amanda Liu <al3623@columbia.edu>
2019-05-29 15:35:19 -0400

    Get smooth motiongrid centered

--- main.js
+++ main.js
@@ -1,18 +1,19 @@
 var ctx = document.getElementById("canvas").getContext("2d");
 
-var levelLData = null;
-var position = [];
-var walls = [];
-var lasers = [];
-var targets = [];
+var levelData = null;
+var grid = [];
+var gridstate = false;
+var startScreenInt = 0;
 
 var lastFrameTime = 0;
 var timedelta = 0;
-var framestep = 1000/1;
-var startScreenInt = 0;
+var speed = 0.009;
+var epsilon = 0.009;
+var drift = false;
+var driftDist = 0;
+var driftDir = -1;
 
-var onStartScreen = true;
-var directions = {up: 1, right: 2, down: 3, left: 4};
+var onStartScreen = false;
 var keys = [0,0,0,0]; //left, up, right, down
 
 var navy = "#184E63";
@@ -24,212 +25,81 @@ var white = "#FFFFFF";
 var egg = "#FDF2E1";
 var grey = "#808080";
 var black = "#000000";
-var scheme4 = [navy,aqua,tea,gold,red,white,egg,grey];
-var colors = scheme4;
+var scheme = [navy,aqua,tea,gold,red,white,egg,grey];
+var colors = scheme;
 
 var me = {
-	currentPos: [],
+	currX: undefined,
+	currY: undefined,
 	travelled: 0,
 }
 
 var levels = {
 		level1: {
 		res: 15,
-		message: "Sometimes you just need to get out of your own way.",
-		start:	[8.0,8.0],
-		laser:	[[8.0,0.0,3]],
-		wall:	[[8,5]],
-		target:	[[8.0,10.0,grey]],
+		startX: 8.0,
+		startY: 8.0,
+		levelgrid: [[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+					[0,0,0,0,1,0,0,0,0,0,0,0,0,0,0],
+					[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+					[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+					[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+					[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+					[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+					[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+					[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+					[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+					[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+					[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+					[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+					[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+					[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]],
 		}
-}
-
-function drawSplitLasers() {
-
-}
-
-function drawLasers(x,y,dir) {
-	ctx.beginPath();
-	ctx.fillStyle = red;
-	ctx.arc((x + 0.5)*(480/levelData.res), (y + 0.5)*(480/levelData.res),
-		5,0,2 * Math.PI);
-	ctx.fill();
-	ctx.closePath();
-	
-	ctx.beginPath();
-	ctx.strokeStyle = red;
-	ctx.lineWidth = 2;
-	ctx.moveTo((x + 0.5)*(480/levelData.res),(y + 0.5)*(480/levelData.res));
-	
-	final_x = undefined;
-	final_y = undefined;
-	in_y = false;
-	in_x = false;
-	x_dev = 0.5;
-	y_dev = 0.5;
-		
-	if (dir === 1) {		// up
-		final_x = x;
-		final_y = 0;
-	} else if (dir === 2) {	// right
-		final_x = 14;
-		final_y = y;
-	} else if (dir === 3) {	// down
-		final_x = x;
-		final_y = 14;
-	} else {				// left
-		final_x = 0;
-		final_y = y;
-	}
-	// check for walls
-	for (i = 0; i < walls.length; i++) {
-		wall = walls[i];
-		if (final_x > x) {
-			if (wall[0] >= x && wall[0] <= final_x) {
-				in_x = true;
-			}
-		} else {
-			if (wall[0] <= x && wall[0] >= final_x) {
-				in_x = true;
-			}
-		}
-		if (final_y > y) {
-			if (wall[1] >= y && wall[1] <= final_y) {
-				in_y = true;
-			}
-		} else {
-			if (wall[1] <= y && wall[1] >= final_y) {
-				in_y = true;
-			}
-		}
-		if (in_x && in_y) {
-			final_x = wall[0];
-			final_y = wall[1];
-			blocking_obj = 0;
-			if (dir === 1 || dir === 3) {
-				y_dev = 0;
-			}
-			if (dir === 2 || dir === 4) {
-				x_dev = 0;
-			}
-		}
-		in_y = false;
-		in_x = false;
-	}
-
-	// check for block
-	block = me.currentPos;
-	if (final_x > x) {
-		if (me.currentPos[0] >= x && me.currentPos[0] <= final_x) {
-			in_x = true;
-		}
-	} else {
-		if (me.currentPos[0] <= x && me.currentPos[0] >= final_x) {
-			in_x = true;
-		}
-	}
-	if (final_y > y) {
-		if (me.currentPos[1] >= y && me.currentPos[1] <= final_y) {
-			in_y = true;
-		}
-	} else {
-		if (me.currentPos[1] <= y && me.currentPos[1] >= final_y) {
-			in_y = true;
-		}
-	}
-	if (in_x && in_y) {
-		final_x = me.currentPos[0];
-		final_y = me.currentPos[1];
-		if (dir === 1 || dir === 3) {
-			y_dev = 0.25;
-		}
-		if (dir === 2 || dir === 4) {
-			x_dev = 0.25;
-		}
-	}
-	in_y = false;
-	in_x = false;
-
-	// check for lasers
-	
-	ctx.lineTo((final_x + x_dev)*(480/levelData.res),
-		(final_y + y_dev)*(480/levelData.res));
-
-	ctx.stroke();
-	ctx.closePath();
-
-}
-
-function drawTarget(x,y,color) {
-	ctx.beginPath();
-	ctx.fillStyle = color;
-	ctx.arc((x + 0.5)*(480/levelData.res), (y + 0.5)*(480/levelData.res),
-		15,0,2 * Math.PI);
-	ctx.fill();
-	ctx.closePath();
 }
 
 function drawGame() {
-	loadLevel("level1");	
-	// draw lasers
-	for (i = 0; i < lasers.length; i++) {
-		drawLasers(lasers[i][0],lasers[i][1],
-			lasers[i][2]);
-	}
+		drawGrid();
 
-	//draw block
-	drawBlock(Math.floor(me.currentPos[0]), 
-		Math.floor(me.currentPos[1]), levelData.res);
-
-	// draw walls
-	for (i = 0; i < walls.length; i++) {
-		cell = walls[i];
-		drawBack(cell[0],cell[1],levelData.res,grey);
-	}
-
-	// draw targets
-	for (i = 0; i < targets.length; i++) {
-		targ = targets[i];
-		drawTarget(targ[0],targ[1],targ[2]);
-	}
+	drawMe(me.currX, me.currY, levelData.res);
 }
-
 
 function loadLevel(level) {
 	levelData = levels[level];
-	me.currentPos = levelData.start.slice();
+	grid = levelData.levelgrid;
+	me.currX = levelData.startX;
+	me.currY = levelData.startY;
 	me.travelled = 0;
-	position = levelData.start;
-	walls = levelData.wall.slice();
-	lasers = levelData.laser.slice();
-	targets = levelData.target;
+	drawGrid();
 }
 
-function drawStartScreen(index) {
-	onStartScreen = true;
-	for (i = 0; i < 5; i++) {
-		ctx.beginPath();
-		ctx.lineWidth = 3;
-		ctx.strokeStyle = colors[(i + index) % 5];
-        ctx.rect(70 - 11*i, 140 - 15*i, 350 + 22*i, 100 + 30*i);
-		ctx.stroke();
-		ctx.closePath();
-	}	
-	ctx.font = "60px Courier";
-	ctx.textAlign="center";
-	ctx.fillStyle = navy;
-	ctx.fillText("prism", 240, 200);
-	
+function drawGrid() {
+	for (i = 0; i < levelData.res; i++) {
+		for (j = 0; j < levelData.res; j++) {
+			// wall	
+			if (grid[i][j] === 1) {
+				ctx.beginPath();
+				ctx.rect(i*(480/levelData.res),j*(480/levelData.res), 
+						(480/levelData.res), (480/levelData.res));
+				ctx.fillStyle = grey;
+				ctx.fill();
+				ctx.closePath();
+			} else {
+				ctx.beginPath();
+				ctx.rect(i*(480/levelData.res),j*(480/levelData.res), 
+						(480/levelData.res), (480/levelData.res));
+				if ((i + j) % 2 === 0) {
+					ctx.fillStyle = tea;
+				} else {
+					ctx.fillStyle = red;
+				}
+				ctx.fill();
+				ctx.closePath();
+			}
+		}
+	}
 }
 
-function drawBack(x,y, res, color) {
-	ctx.beginPath();
-	ctx.fillStyle = color;
-	ctx.rect(x*(480/res), y*(480/res), (480/res), (480/res));
-	ctx.fill();
-	ctx.closePath();
-}
-
-function drawBlock(x, y, res) {
+function drawMe(x, y, res) {
 	ctx.beginPath();
 	ctx.rect((x+0.25)*(480/res), (y+0.25)*(480/res), (480/res)/2, (480/res)/2);
 	ctx.fillStyle = grey;
@@ -244,36 +114,69 @@ function update(timestamp) {
 	if (timedelta < 600 && onStartScreen) {
 		window.requestAnimationFrame(update);
 	} else {
-		if (onStartScreen) {
-			timedelta = 0;
-			clear("#FFFFFF");
-			drawStartScreen(startScreenInt);
-			startScreenInt = (startScreenInt + 1) % 5;
-		} else {
-			timedelta = 0;
-			clear("#FFFFFF");
-			updatePositions();
-			drawGame();
-		}
-		window.requestAnimationFrame(update);
+		clear("#FFFFFF");
+		updatePositions(timedelta);
+		timedelta = 0;
+		drawGame();
+	}
+	window.requestAnimationFrame(update);
+}
+
+function centerMe(dir) {
+	dist = 0;
+	if (dir === 0) {			// left
+		goalX = Math.floor(me.currX);
+		dist = me.currX - goalX;
+	} else if (dir === 1) {		// up
+		goalY = Math.floor(me.currY);
+		dist = me.currY - me.currY;
+	} else if (dir === 2) {		// right
+		goalX = Math.ceil(me.currX);
+		dist = goalX - me.currX;
+	} else {					// down
+		goalY = Math.ceil(me.currY);
+		dist = goalY - me.currY;
+	}
+	if (dist > 0) {
+		drift = true;
+		driftDist = dist;
+		driftDir = dir;
 	}
 }
 
-function updatePositions() {
-		var cube = position;	
-		var x = cube[0];
-		var y = cube[1];
-		if (keys[0] === 1 && x > 0)  {		// left
-			position[0] -= 1;
-		} else if (keys[1] === 1 && y > 0)  {	// up
-			position[1] -= 1;
-		} else if (keys[2] === 1 && x < levelData.res-1)  {	// right
-			position[0] += 1;
-		} else if (keys[3] === 1 && y < levelData.res-1) {	// down
-			position[1] += 1;
+function updatePositions(delta) {
+		x = me.currX;
+		y = me.currY;
+		if ((keys[0] === 1 
+				|| (drift && driftDir === 0)) && x > 0)  {		// left
+			driftDist -= delta * speed;
+			me.currX -= delta * speed;
+		} else if ((keys[1] === 1 
+				|| (drift && driftDir === 1)) && y > 0)  {	// up
+			driftDist -= delta * speed;
+			me.currY -= delta * speed;
+		} else if ((keys[2] === 1 
+				|| (drift && driftDir === 2)) 
+				&& x < levelData.res-1)  {	// right
+			driftDist -= delta * speed;
+			me.currX += delta * speed;
+		} else if ((keys[3] === 1
+				|| (drift && driftDir === 3)) 
+				&& y < levelData.res-1) {	// down
+			driftDist -= delta * speed;
+			me.currY += delta * speed;
 		} else {
-			
+			// wat
 		}
+
+		if (drift && driftDist <= epsilon) {
+			me.currX = Math.round(me.currX);
+			me.currY = Math.round(me.currY);
+			drift = false;
+			driftDist = 0;
+			driftDir = -1;
+		}
+	
 }
 
 function clear(color) {
@@ -305,21 +208,22 @@ function keydown(e) {
 }
 
 function keyup(e) {
-	if (onStartScreen) {
-		onStartScreen = false;
-	}
 	switch(e.keyCode) {
 		case 37:	//left
 			keys[0] = 0;
+			centerMe(0);
 			break;
 		case 38: 	//up
 			keys[1] = 0;
+			centerMe(1);
 			break;
 		case 39:	//right
 			keys[2] = 0;
+			centerMe(2);
 			break;
 		case 40:	//down
 			keys[3] = 0;
+			centerMe(3);
 			break;
 		case 82:
 			//reset
@@ -330,8 +234,8 @@ function keyup(e) {
 	e.preventDefault();
 }
 
+loadLevel("level1");
 document.addEventListener("keydown", keydown, false);
 document.addEventListener("keyup", keyup, false);
 
-drawStartScreen(startScreenInt++);
 window.requestAnimationFrame(update);
